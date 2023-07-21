2fd73de5b8a78a5c5fd3e109a4db427478a72b22
ruthrakotti24 <ruthrakotti24@gmail.com>
2023-07-21 22:23:03 +0530

    new bracnch commmit

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+#hii this is  a new comment
+
+
 table_number = int(input("Enter the number for the multiplication table: "))
 for i in range(1, 11):
     print(f"{table_number} * {i} = {table_number * i}")
