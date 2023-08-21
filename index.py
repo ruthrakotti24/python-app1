f3b7f62670135af6c82cb548f58a63025a0c10e7
Ruthrakotti Lingavel <101353999+ruthrakotti24@users.noreply.github.com>
2023-08-21 13:01:04 +0530

    Delete index.py

--- index.py
+++
@@ -1,6 +0,0 @@
-#hii this is  a new comment
-
-
-table_number = int(input("Enter the number for the multiplication table: "))
-for i in range(1, 11):
-    print(f"{table_number} * {i} = {table_number * i}")
